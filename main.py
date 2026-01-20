1808d7bfaaf01fa2a1d1dfa89a82f3ebdfbda64f
urgle55 <caseytravissmith@gmail.com>
2026-01-20 15:52:38 -0800

    Implemented data split in main.py. Completed train_random_forest's run.py and implemented in main.py. Tested both - results in W&B

--- main.py
+++ main.py
@@ -79,10 +79,17 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+            f"{config['main']['components_repository']}/train_val_test_split",
+            "main",
+            env_manager="conda",
+            parameters={
+                "input": "clean_sample.csv:latest",
+                "test_size": config["modeling"]["test_size"],
+                "random_seed": config["modeling"]["random_seed"],
+                "stratify_by": config["modeling"]["stratify_by"]
+            },
+        )
 
         if "train_random_forest" in active_steps:
 
@@ -94,11 +101,20 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                "src/train_random_forest",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                },
+            )
 
         if "test_regression_model" in active_steps:
 
