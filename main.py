6d56515124bdba0ee21dfea4b8a8d82619d616de
urgle55 <caseytravissmith@gmail.com>
2026-01-20 15:14:03 -0800

    Added data tests for row count and price range to test_data.py and implemented it in main.py

--- main.py
+++ main.py
@@ -63,15 +63,20 @@ def go(config: DictConfig):
                     "max_price": config['etl']['max_price']
                 }
             )
-            # Implement here #
-            ##################
-            pass
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                "src/data_check",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -87,3 +87,22 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data: pd.DataFrame) -> None:
+    """Test that the dataset size is reasonable (not tiny or huge).
+    
+    Args:
+        data:Imput DataFrame to test
+    """
+
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float) -> None:
+    """Test that all prices are within expected range
+    
+    Args:
+        data: Input DataFrame to test
+        min_price: Minimum allowed price
+        max_price: Maximum allowed price
+    """
+    assert data['price'].between(min_price, max_price).all()
